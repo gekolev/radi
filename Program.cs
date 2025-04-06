5f68e591b42497dac8c01cf1fae4aaa712e15e5b
MyonLul <166629202+MyonLul@users.noreply.github.com>
2025-04-06 22:44:30 +0300

    Excersise 11

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int[] numbers = new int [10];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"Enter integer {i + 1}: ");
+    numbers[i] = int.Parse(Console.ReadLine()); 
+}
+
+int largest = numbers[0];
+foreach (int num in numbers)
+{
+if (num > largest)
+largest = num; 
+}
--- obj/Debug/net9.0/radi.AssemblyInfo.cs
+++ obj/Debug/net9.0/radi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("radi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3760cb6c3136f403c39db3b7337a9bf9d6b0372")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02711a19275a3934793f1b2bb2aa301174f313ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("radi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("radi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
